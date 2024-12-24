8caeb8efb20846f681a20d35d857f0d8faed1e9f
Davis Yu <davisyu@taiwanmobile.com>
2024-12-24 11:54:15 +0800

    feat

--- app/_components/PostList2.jsx
+++ app/_components/PostList2.jsx
@@ -5,29 +5,29 @@ import { useEffect, useState } from "react";
 import { Post } from "@/app/_components";
 
 const PostList2 = () => {
-    const [post, setPost] = useState(null);
+  const [post, setPost] = useState(null);
 
-    useEffect(() => {
-        (async () => {
-            const response = await fetch("/api/post", {
-                method: "POST",
-                body: JSON.stringify({
-                    delay: 3500,
-                }),
-            });
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("/api/post", {
+        method: "POST",
+        body: JSON.stringify({
+          delay: 3500,
+        }),
+      });
 
-            const { data } = await response.json();
-            setPost(data);
-        })();
-    }, []);
+      const { data } = await response.json();
+      setPost(data);
+    })();
+  }, []);
 
-    return (
-        <ul>
-            {post?.map((item, index) => (
-                <Post key={index} />
-            ))}
-        </ul>
-    );
+  return (
+    <ul>
+      {post?.map((item, index) => (
+        <Post key={index} />
+      ))}
+    </ul>
+  );
 };
 
 export default PostList2;
